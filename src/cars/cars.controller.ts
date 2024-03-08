e1c92ba194d67e933b407a33a148e232a5798c8a
Jayro Salazar <megajjks@gmail.com>
2024-03-07 22:13:11 -0600

    creat module and controller of cars

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+
+  private cars = ['Toyota', 'Honda', 'Jeep'];
+
+  @Get()
+  getAllCars() {
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById( @Param('id') id:string) {
+    console.log({ id: id, name:this.cars[id] });
+    return {
+      id: id,
+      name: this.cars[id]
+    }
+  }
+}
