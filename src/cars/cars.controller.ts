016d9d5aac25a5b035b1567d5aa718168b8e337a
Jayro Salazar <megajjks@gmail.com>
2024-03-11 22:34:09 -0600

    creat crud dummy car

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,21 +1,41 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post, Body, Patch, Delete } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
-  private cars = ['Toyota', 'Honda', 'Jeep'];
+  constructor(
+    private readonly carsService: CarsService,
+  ){}
 
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.carsService.findAll();
   }
 
   @Get(':id')
-  getCarById( @Param('id') id:string) {
-    console.log({ id: id, name:this.cars[id] });
+  getCarById( @Param('id', ParseIntPipe) id:number) {
+    return this.carsService.findOneById(Number(id))
+  }
+
+  @Post()
+  createCar( @Body() body:any){
+    return body;
+  }
+
+  @Patch(':id')
+  updateCar( 
+      @Param('id', ParseIntPipe) id:number,
+      @Body() body:any
+    ){
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar( @Param('id', ParseIntPipe) id:number){
     return {
-      id: id,
-      name: this.cars[id]
+      method: 'deleted',
+      id
     }
   }
 }
