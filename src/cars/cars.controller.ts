d9e8c45df6f096e0d425b6a7094ee93bb5f249dd
Jayro Salazar <megajjks@gmail.com>
2024-03-12 22:17:53 -0600

    add DTO and UUID

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param, ParseIntPipe, Post, Body, Patch, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch, Delete, ParseUUIDPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -14,25 +15,25 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById( @Param('id', ParseIntPipe) id:number) {
-    return this.carsService.findOneById(Number(id))
+  getCarById( @Param('id', new ParseUUIDPipe({ version:'4'})) id:string) {
+    return this.carsService.findOneById(id)
   }
 
   @Post()
-  createCar( @Body() body:any){
-    return body;
+  createCar( @Body() createCarDto:CreateCarDto){
+    return createCarDto;
   }
 
   @Patch(':id')
   updateCar( 
-      @Param('id', ParseIntPipe) id:number,
+      @Param('id') id:string,
       @Body() body:any
     ){
     return body;
   }
 
   @Delete(':id')
-  deleteCar( @Param('id', ParseIntPipe) id:number){
+  deleteCar( @Param('id') id:string){
     return {
       method: 'deleted',
       id
